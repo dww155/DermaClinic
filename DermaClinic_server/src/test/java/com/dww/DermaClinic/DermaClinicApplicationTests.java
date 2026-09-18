99ba2e068c4e382a35965d371f8789525f9c4ccd
dww155 <trandinhkhanhdu2005@gmail.com>
2026-09-19 00:17:50 +0700

    init server

---
+++ DermaClinic_server/src/test/java/com/dww/DermaClinic/DermaClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dww.DermaClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermaClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
